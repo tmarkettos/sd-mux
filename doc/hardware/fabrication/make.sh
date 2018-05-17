7d2b89157d65471adeda5cde6f913ab2587aa658
Theo Markettos <git+atm26@cl.cam.ac.uk>
2018-05-17 21:37:59 +0100

    Remove origin mark that confuses DirtyPCBs

---
+++ doc/hardware/fabrication/make.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+VER=4.0
+DIR=sd-mux-atm26-v4
+PRJ=sd-mux
+
+rm sd-mux-v${VER}.zip
+mv $DIR/$PRJ-Edge.Cuts.gm1 $DIR/$PRJ-Edge.Cuts.gbr
+mv $DIR/$PRJ.drl $DIR/$PRJ.txt
+zip -rv9 $PRJ-$VER.zip $DIR
