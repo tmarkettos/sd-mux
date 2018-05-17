2796707380c4c43574f09052b924bce081bbe63d
Theo Markettos <git+atm26@cl.cam.ac.uk>
2018-05-17 21:41:32 +0100

    Rename fabrication files to match existing

--- doc/hardware/fabrication/make.sh
+++ doc/hardware/fabrication/make.sh
@@ -4,7 +4,7 @@ VER=4.0
 DIR=sd-mux-atm26-v4
 PRJ=sd-mux
 
-rm sd-mux-v${VER}.zip
+rm sd-mux-fabrication0v${VER}.zip
 mv $DIR/$PRJ-Edge.Cuts.gm1 $DIR/$PRJ-Edge.Cuts.gbr
 mv $DIR/$PRJ.drl $DIR/$PRJ.txt
-zip -rv9 $PRJ-v$VER.zip $DIR
+zip -rv9 $PRJ-fabrication-v$VER.zip $DIR
